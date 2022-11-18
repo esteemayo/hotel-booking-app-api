4a72be3e63dc2560dae27b61913f8bd9cfe8c4e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 15:41:12 +0100

    feat: create hotel model

---
+++ models/Hotel.js
@@ -0,0 +1,62 @@
+import slugify from 'slugify';
+import { model, models, Schema } from 'mongoose';
+
+const hotelSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Hotel must have a name'],
+    trim: true,
+  },
+  slug: String,
+  type: {
+    type: String,
+    required: [true, 'Hotel must have a type'],
+  },
+  city: {
+    type: String,
+    required: [true, 'Hotel must belong to a city'],
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: [true, 'Hotel must have an address'],
+    trim: true,
+  },
+  distance: {
+    type: String,
+    required: [true, 'Hotel must have a distance field'],
+    trim: true,
+  },
+  photos: {
+    type: [String],
+  },
+  desc: {
+    type: String,
+    required: [true, 'Hotel must have a description'],
+    trim: true,
+  },
+  rating: {
+    type: Number,
+    min: [0, 'Rating must be above 0'],
+    max: [5, 'Rating must be below 5'],
+  },
+  rooms: {
+    type: [String],
+  },
+  cheapestPrice: {
+    type: Number,
+    required: [true, 'Cheapest price must be supplied'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+const Hotel = models.Hotel || model('Hotel', hotelSchema);
+
+export default Hotel;
