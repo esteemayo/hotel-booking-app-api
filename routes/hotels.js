dac1c4db0674d2bd53fcc32238b81e2df083ae52
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 15:41:54 +0100

    feat: create hotel endpoints

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import * as hotelController from '../controllers/hotelController.js';
 
 const router = express.Router();
+
+router.get('/details/:slug', hotelController.getHotelBySlug);
+
+router
+  .route('/')
+  .get(hotelController.getHotels)
+  .post(hotelController.createHotel);
+
+router
+  .route('/:id')
+  .get(hotelController.getHotelById)
+  .patch(hotelController.updateHotel)
+  .delete(hotelController.deleteHotel);
 
 export default router;
