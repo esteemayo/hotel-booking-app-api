eec351a7dde4b3017709abd1d362b9183e9ad38f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 23:13:05 +0100

    add register user api endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import * as userController from '../controllers/userController';
 
 const router = express.Router();
+
+router.post('/register', userController.register);
 
 export default router;
