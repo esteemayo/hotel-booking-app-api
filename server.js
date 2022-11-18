6a164fe534d0a724fce436872c0b6c53a4a7cae6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 14:39:42 +0100

    configured mongoose and connected to the server

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import app from './app';
+import 'colors';
+
+import app from './app.js';
+
+dotenv.config({ path: './config.env' });
+
+const connectDB = async () => {
+  try {
+    const cons = await mongoose.connect(process.env.DATABASE_LOCAL);
+    console.log(`Connected to MongoDB ↔ ${cons.connection.port}`.gray.bold);
+  } catch (err) {
+    throw err;
+  }
+};
+
+app.set('port', process.env.PORT || 8800);
+
+const server = app.listen(app.get('port'), () => {
+  connectDB();
+  console.log(`App listening on port ↔ ${server.address().port}`.green.bold);
+});
 
 dotenv.config({ path: './config.env ' });
