9c2d5d9e7893c5c735e304d435347f7f5fd94910
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 16:24:58 +0100

    feat: add hotels get all api endpoint

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -3,7 +3,16 @@ import asyncHandler from 'express-async-handler';
 
 import Hotel from '../models/Hotel.js';
 
-export const getHotels = asyncHandler(async (req, res, next) => { });
+export const getHotels = asyncHandler(async (req, res, next) => {
+  const hotels = await Hotel.find();
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: hotels.length,
+    hotels,
+  })
+});
 
 export const getHotelById = asyncHandler(async (req, res, next) => { });
 
