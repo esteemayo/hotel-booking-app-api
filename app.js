f15a5108a282171ff7327844f2250cd64c54d5f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 14:46:54 +0100

    modified

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 import express from 'express';
+import morgan from 'morgan';
 
+// start express app
 const app = express();
+
+// global middlewares
+// implement CORS
+
+// Access-Control-Allow-Origin
+
+// set security HTTP headers
+
+// development logging
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'));
+}
+
+// limit request from same api
+
+// body Parser, reading data from body into req.body
+
+// cookie parser middleware
+
+// data sanitization against NoSQL query injection
+
+// data sanitization against XSS
+
+// prevent parameter pollution
+
+// compression middleware
+
+// test middleware
+
+// api routes middleware
 
 export default app;
