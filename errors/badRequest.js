f62a5638d39178df3ad43373917b08121ec7b223
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-18 21:57:43 +0100

    added bad requeset error

---
+++ errors/badRequest.js
@@ -0,0 +1,13 @@
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customAPIError.js';
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+export default BadRequestError;
