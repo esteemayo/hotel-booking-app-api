14b5c351a97a222c21b2ee870471ab62ec6ad1b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 01:56:53 +0100

    feat: save token to cookie

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,5 +1,16 @@
 const createSendToken = (user, status, req, res) => {
   const token = user.generateAuthToken();
+
+  res.cookie('access_token', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: true,
+    signed: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
   const { password, role, ...rest } = user._doc;
 
   res.status(status).json({
