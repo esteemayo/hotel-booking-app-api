98c180868f8ef4f137af5ab845e9cd79e26f28d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 01:20:58 +0100

    feat: add user login api endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import * as authController from '../controllers/authController.js';
 
 const router = express.Router();
+
+router.get('/login', authController.login);
 
 export default router;
