3bdc74f3ef572efe576fa214895dfd8d051e88cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 02:19:42 +0100

    feat: add crud user api logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import NotFoundError from '../errors/notFound.js';
 import createSendToken from '../utils/createSendToken.js';
 
 export const register = asyncHandler(async (req, res, next) => {
@@ -11,3 +12,81 @@ export const register = asyncHandler(async (req, res, next) => {
     createSendToken(user, StatusCodes.CREATED, req, res);
   }
 });
+
+export const getUsers = asyncHandler(async (req, res, next) => {
+  const user = await User.find();
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: user.length,
+    user,
+  })
+});
+
+export const getUser = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID → ${userId}`)
+    );
+  }
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    user,
+  });
+});
+
+export const updateUser = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findByIdAndUpdate(
+    userId,
+    { $set: { ...req.body } },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID → ${userId}`)
+    );
+  }
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    user,
+  });
+});
+
+export const deleteuser = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.params;
+
+  const user = await User.findByIdAndDelete(userId);
+
+  if (!user) {
+    return next(
+      new NotFoundError(`There is no user found with the given ID → ${userId}`)
+    );
+  }
+
+  res.status(StatusCodes.NO_CONTENT).json({
+    status: 'success',
+    user: null,
+  });
+});
+
+export const createUser = (req, res) => {
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'fail',
+    message: `This route is not defined! Please use ${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/register`,
+  });
+};
