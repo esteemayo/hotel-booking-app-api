9c886a2e6a7b88f457d7ed553d2009312a2b3d73
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 01:23:26 +0100

    fix: change http method to post

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ import * as authController from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.get('/login', authController.login);
+router.post('/login', authController.login);
 
 export default router;
