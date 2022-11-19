bb6e719631ad7605e1f46cad0fe10f1c88d5dc11
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 01:32:13 +0100

    feat: refactor

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import createSendToken from '../utils/createSendToken.js';
 
 export const register = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
 
   if (user) {
-    res.status(StatusCodes.CREATED).json({
-      status: 'success',
-      user,
-    });
+    createSendToken(user, StatusCodes.CREATED, req, res);
   }
 });
