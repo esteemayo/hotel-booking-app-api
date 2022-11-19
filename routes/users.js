621310640115a03633c6f52a1edc9594148ddeb0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 02:19:22 +0100

    feat: add user crud api logic

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,15 @@ const router = express.Router();
 
 router.post('/register', userController.register);
 
+router
+  .route('/')
+  .get(userController.getUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteuser);
+
 export default router;
