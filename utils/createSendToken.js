a44aa3bf675945669b47d6caebf6483a9b8917f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-19 01:48:07 +0100

    feat: generate auth token for user

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,8 +1,10 @@
 const createSendToken = (user, status, req, res) => {
+  const token = user.generateAuthToken();
   const { password, role, ...rest } = user._doc;
 
   res.status(status).json({
     status: 'success',
+    token,
     ...rest,
   });
 };
