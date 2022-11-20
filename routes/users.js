fcba18ab86b36317fbea6a4f80a005cd5ee0bf96
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-20 14:28:42 +0100

    feat: add auth middleware to api endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,23 @@
 import express from 'express';
+
+import authMiddleware from '../middlewares/authMiddleware.js';
 import * as userController from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', userController.register);
 
+router.use(authMiddleware.protect);
+
 router
   .route('/')
-  .get(userController.getUsers)
+  .get(authMiddleware.restrictTo('admin'), userController.getUsers)
   .post(userController.createUser);
 
 router
   .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteuser);
+  .get(authMiddleware.verifyUser, userController.getUser)
+  .patch(authMiddleware.restrictTo('admin'), userController.updateUser)
+  .delete(authMiddleware.restrictTo('admin'), userController.deleteuser);
 
 export default router;
