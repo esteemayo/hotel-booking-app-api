278ab92c51ed43261c954e59a9794193c3c736b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-20 13:15:57 +0100

    added auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/User.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
+
+export default asyncHandler(async (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return next(
+      new UnauthenticatedError(
+        'You are not logged in! Please log in to get access'
+      )
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new UnauthenticatedError(
+        'The user belonging to this token does no longer exist'
+      )
+    );
+  }
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new UnauthenticatedError(
+        'User recently changed password! Please log in again'
+      )
+    );
+  }
+
+  req.user = currentUser;
+  next();
+});
