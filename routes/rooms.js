30c4bf83810b34799a06c5a649c83f4d25ca5582
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 00:56:37 +0100

    feat: add all rooms api endpoints

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,31 @@
 import express from 'express';
 
+import authMiddiware from '../middlewares/authMiddleware.js';
+import * as roomController from '../controllers/roomController.js';
+
 const router = express.Router();
+
+router
+  .route('/')
+  .get(roomController.getRooms)
+  .post(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    roomController.createRoom
+  );
+
+router
+  .route('/:id')
+  .get(roomController.getRoom)
+  .patch(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    roomController.updateRoom
+  )
+  .delete(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    roomController.deleteRoom
+  );
 
 export default router;
