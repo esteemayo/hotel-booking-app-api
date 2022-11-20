5ec906d1138de3b54d17819780483ba23ed06dbf
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 00:19:38 +0100

    feat: add authorization to some hotels api endpoints

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,4 +1,6 @@
 import express from 'express';
+
+import authMiddiware from '../middlewares/authMiddleware.js';
 import * as hotelController from '../controllers/hotelController.js';
 
 const router = express.Router();
@@ -8,12 +10,24 @@ router.get('/details/:slug', hotelController.getHotelBySlug);
 router
   .route('/')
   .get(hotelController.getHotels)
-  .post(hotelController.createHotel);
+  .post(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    hotelController.createHotel
+  );
 
 router
   .route('/:id')
   .get(hotelController.getHotelById)
-  .patch(hotelController.updateHotel)
-  .delete(hotelController.deleteHotel);
+  .patch(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    hotelController.updateHotel
+  )
+  .delete(
+    authMiddiware.protect,
+    authMiddiware.restrictTo('admin'),
+    hotelController.deleteHotel
+  );
 
 export default router;
