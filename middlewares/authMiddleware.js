69d291541cf8485068ca36744cd8eb9f1c908121
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-20 14:26:24 +0100

    feat: modify

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -50,7 +50,7 @@ const restrictTo =
     (req, res, next) => {
       if (!roles.includes(req.user.role)) {
         return next(
-          new ForbiddenError('You don not have permission to perform this action')
+          new ForbiddenError('You do not have permission to perform this action')
         );
       }
       next();
