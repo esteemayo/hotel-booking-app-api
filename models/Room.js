39f3244f135158e8243033fd9f0d611f331d72f8
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 00:30:56 +0100

    create room model

---
+++ models/Room.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const roomSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A room must have a title'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A room must have a price'],
+  },
+  maxPeople: {
+    type: Number,
+    required: [true, 'A room maxPeople field must be specified'],
+  },
+  desc: {
+    type: String,
+    required: [true, 'A room must have a description'],
+    trim: true,
+  },
+  roomNumbers: [
+    {
+      number: Number,
+      unavailableDates: { type: [Date] },
+    }
+  ],
+}, {
+  timestamps: true,
+});
+
+const Room = mongoose.models.Room || mongoose.model('Room', roomSchema);
+
+export default Room;
