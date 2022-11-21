091b835c6b8c8d09e3fbd8dbae1adae1c6d4b578
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 15:39:51 +0100

    feat: add reset-password api endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,6 @@ router.post('/login', authController.login);
 
 router.post('/forgot-password', authController.forgotPassword);
 
+router.patch('/reset-password/:token', authController.resetPassword);
+
 export default router;
