0c5dcf78599f16473ce2383089aef77c3482d7e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 21:03:43 +0100

    feat: add getUserStats method

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,25 +31,7 @@ export const getUsers = asyncHandler(async (req, res, next) => {
 });
 
 export const getUserStats = asyncHandler(async (req, res, next) => {
-  const now = new Date();
-  const lastMonth = new Date(now.setMonth(now.getMonth() - 1));
-
-  const stats = await User.aggregate([
-    {
-      $match: { createdAt: { $gte: lastMonth } },
-    },
-    {
-      $project: {
-        month: { $month: '$createdAt' },
-      },
-    },
-    {
-      $group: {
-        _id: '$month',
-        total: { $sum: 1 },
-      },
-    },
-  ]);
+  const stats = await User.getUserStats();
 
   res.status(StatusCodes.OK).json({
     status: 'success',
