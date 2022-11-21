e209728759d9ac746d1d501c53306701bddca748
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 20:42:58 +0100

    feat: formatted

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,10 @@ router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/reset-password/:token', authController.resetPassword);
 
-router.patch('/update-my-password', authMiddiware.protect, authController.updatePassword);
+router.patch(
+  '/update-my-password',
+  authMiddiware.protect,
+  authController.updatePassword
+);
 
 export default router;
