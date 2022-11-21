9a8522f1d7effff6c79494df54809bc1274f3599
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 14:12:24 +0100

    feat: add new api endpoints

--- routes/hotels.js
+++ routes/hotels.js
@@ -5,6 +5,10 @@ import * as hotelController from '../controllers/hotelController.js';
 
 const router = express.Router();
 
+router.get('/count-by-city', hotelController.countByCity);
+
+router.get('/count-by-type', hotelController.countByType);
+
 router.get('/details/:slug', hotelController.getHotelBySlug);
 
 router
