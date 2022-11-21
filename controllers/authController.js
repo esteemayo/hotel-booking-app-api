a8997c144d269c98f63c7d06ffd1593d92a4bdb0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-21 15:18:16 +0100

    feat: add forgot password api logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import sendMail from '../utils/email.js';
+import NotFoundError from '../errors/notFound.js';
 import BadRequestError from '../errors/badRequest.js';
-import UnauthenticatedError from '../errors/unauthenticated.js';
+import CustomAPIError from '../errors/customAPIError.js';
 import createSendToken from '../utils/createSendToken.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
 
 export const login = asyncHandler(async (req, res, next) => {
   const { username, password } = req.body;
@@ -19,4 +22,66 @@ export const login = asyncHandler(async (req, res, next) => {
   }
 
   createSendToken(user, StatusCodes.OK, req, res);
+});
+
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return next(new BadRequestError('Please provide your email address'));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(
+      new NotFoundError('There is no user with the email address')
+    );
+  }
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/reset-password/${resetToken}`;
+
+  const message = `
+    Hi ${user.name},
+    There was a request to change your password!
+    If you did not make this request then please ignore this email.
+    Otherwise, please click this link to change your password: ${resetURL}
+  `;
+
+  const html = `
+    <div style='background: #f7f7f7; color: #333; padding: 50px; text-align: left;'>
+      <h3>Hi ${user.name},</h3>
+      <p>There was a request to change your password!</p>
+      <p>If you did not make this request then please ignore this email.</p>
+      <p>Otherwise, please click this link to change your password: 
+        <a href='${resetURL}'>Reset my password →</a>
+      </p>
+    </div>
+  `;
+
+  try {
+    await sendMail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 minutes)',
+      message,
+      html,
+    });
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: `Token sent to email: ${user.email}`,
+    });
+  } catch (err) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new CustomAPIError('There was an error sending the email. Please try again later')
+    );
+  }
 });
