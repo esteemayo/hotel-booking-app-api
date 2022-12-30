ab6bcf6b2de12c8d19bed252ae34d05d2c3501da
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-30 17:33:39 +0100

    feat: create import dev data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+import User from '../../models/User.js';
+import Room from '../../models/Room.js';
+import Hotel from '../../models/Hotel.js';
+import connectDB from '../../config/db.js';
+
+dotenv.config({ path: './config.env' });
+
+connectDB();
+
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const rooms = JSON.parse(fs.readFileSync(`${__dirname}/rooms.json`, 'utf-8'));
+const hotels = JSON.parse(fs.readFileSync(`${__dirname}/hotels.json`, 'utf-8'));
+
+const loadData = async () => {
+  try {
+    await Room.create(rooms);
+    await Hotel.create(hotels);
+    await User.create(users, { validateBeforeSave: false });
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+};
+
+const removeData = async () => {
+  try {
+    await User.deleteMany();
+    await Room.deleteMany();
+    await Hotel.deleteMany();
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+};
+
+if (process.argv[2] === '--load') {
+  loadData();
+} else if (process.argv[2] === '--remove') {
+  removeData();
+}
