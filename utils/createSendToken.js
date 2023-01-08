2e06485c36769f949b3e9a3364abedae77f726f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-08 11:31:18 +0100

    fix: modify

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -3,7 +3,7 @@ const createSendToken = (user, status, req, res) => {
 
   res.cookie('access_token', token, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000
     ),
     httpOnly: true,
     sameSite: true,
